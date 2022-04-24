00fb59a868572eba4924f5acc848ce2c112fcff6
ahmadaldali88 <ahmadaldali88@gmail.com>
2022-04-24 20:30:39 +0300

    update composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '802b7400b52745379ae435f5d0de478c8b1c213f',
+        'name' => 'ahmad/filter-lists',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ahmad/filter-lists' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '802b7400b52745379ae435f5d0de478c8b1c213f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
